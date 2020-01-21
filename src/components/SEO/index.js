c7cf952f08ee8dc19d57f3dd13ece07236dd146b
misioq23 <mi23chal@gmail.com>
2020-01-21 21:23:42 +0100

    add SEO component

---
+++ src/components/SEO/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const SEO = ({ title, description, lang, meta }) => {
+	const { site } = useStaticQuery(
+		graphql`
+			query {
+				site {
+					siteMetadata {
+						title
+						description
+						author
+					}
+				}
+			}
+		`,
+	);
+
+	const metaDescription = description || site.siteMetadata.description;
+
+	return (
+		<Helmet
+			htmlAttributes={{
+				lang,
+			}}
+			title={title}
+			titleTemplate={`%s | ${site.siteMetadata.title}`}
+			meta={[
+				{
+					name: 'description',
+					content: metaDescription,
+				},
+				{
+					property: 'og:title',
+					content: title,
+				},
+				{
+					property: 'og:description',
+					content: metaDescription,
+				},
+				{
+					property: 'og:type',
+					content: 'website',
+				},
+			].concat(meta)}
+		/>
+	);
+};
+
+SEO.propTypes = {
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string,
+	lang: PropTypes.string,
+	meta: PropTypes.arrayOf(PropTypes.object),
+};
+
+SEO.defaultProps = {
+	description: '',
+	lang: 'pl',
+	meta: [],
+};
+
+export default SEO;
