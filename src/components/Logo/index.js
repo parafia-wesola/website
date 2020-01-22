9c3d833c740031bef366b04e40556678d3e4433c
misioq23 <mi23chal@gmail.com>
2020-01-22 14:38:33 +0100

    add Logo component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import ConditionalLink from 'components/Conditional';
+import { LogoWrapper, LogoText } from './styles';
+
+const Logo = ({ to, image, text, className }) => (
+	<LogoWrapper as={ConditionalLink} to={to} className={className}>
+		{image && <Img fixed={image} />}
+		<LogoText>{text}</LogoText>
+	</LogoWrapper>
+);
+
+Logo.propTypes = {
+	to: PropTypes.string,
+	image: PropTypes.shape(),
+	text: PropTypes.string,
+	className: PropTypes.string,
+};
+
+Logo.defaultProps = {
+	to: null,
+	image: null,
+	text: null,
+	className: null,
+};
+
+export default Logo;
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const LogoWrapper = styled.a`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin: 0;
+	padding: 0;
+	color: ${({ theme }) => theme.colors.dark};
+	font-size: 1em;
+	text-align: left;
+	text-decoration: none;
+	background: transparent;
+	border: none;
+`;
+
+export const LogoText = styled.h1`
+	margin: 0;
+	font-weight: 600;
+	font-size: 1em;
+	line-height: 1.15;
+	white-space: pre;
+	pointer-events: none;
+`;
