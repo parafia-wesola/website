1e6cdd65b286e2464bb6e062f0e092701d84dcd4
misioq23 <mi23chal@gmail.com>
2020-01-27 15:27:28 +0100

    add gatsby remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-styled-components',
 		'gatsby-transformer-json',
+		'gatsby-transformer-remark',
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
