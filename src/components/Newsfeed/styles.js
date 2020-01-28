0b4ebe33bbc1dcdb6dbd2ba8c00b9ec8147977f4
misioq23 <mi23chal@gmail.com>
2020-01-28 15:10:49 +0100

    add NewsFeed component

---
+++ src/components/Newsfeed/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { sideLengthMedium, sideLengthLarge, gapLength } from './config';
+
+export const NewsFeedWrapper = styled.div`
+	display: grid;
+	grid-gap: ${gapLength};
+	grid-template-columns: repeat(auto-fit, ${sideLengthMedium});
+	justify-content: center;
+`;
+
+export const MediumItem = styled.div`
+	grid-row-end: span 1;
+	grid-column-end: span 2;
+	min-height: ${sideLengthMedium};
+`;
+
+export const LargeItem = styled.div`
+	grid-row-end: span 2;
+	grid-column-end: span 2;
+	height: ${sideLengthLarge};
+`;
