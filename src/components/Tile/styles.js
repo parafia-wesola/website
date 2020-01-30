4a8f632e51616bd92e540e400dfdd3efa55f49d9
misioq23 <mi23chal@gmail.com>
2020-01-30 07:05:27 +0100

    add hover animation on images & title

--- src/components/Tile/styles.js
+++ src/components/Tile/styles.js
@@ -7,22 +7,43 @@ export const Wrapper = styled.div`
 	justify-content: center;
 	padding: 1em 1rem;
 	text-decoration: none;
-	transition: all 0.2s ease-in-out;
+	transition: all 0.5s ease-in-out;
 
 	&:hover {
-		transform: scale(1.1);
+		transform: scale(1.05);
+
+		h3 {
+			color: ${({ theme }) => theme.colors.bright};
+		}
+
+		div::before {
+			opacity: 0;
+		}
 	}
 `;
 
 export const Title = styled.h3`
 	margin: 0;
 	padding: 0.75em;
-	color: ${({ theme }) => theme.colors.bright};
+	color: #ccc;
 	font-weight: 600;
 	font-size: 1.15em;
+	transition: color 0.5s ease-in-out;
 `;
 
 export const ImageWrapper = styled.div`
+	position: relative;
 	width: 200px;
 	height: 200px;
+
+	&::before {
+		position: absolute;
+		z-index: 1;
+		width: 100%;
+		height: 100%;
+		background: #000;
+		opacity: 0.4;
+		transition: opacity 0.5s ease-in-out;
+		content: '';
+	}
 `;
