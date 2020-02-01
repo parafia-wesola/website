21864be019f9c981d60f6d08afdee3d94b63dd57
misioq23 <mi23chal@gmail.com>
2020-02-01 02:25:53 +0100

    add Annoucement Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Layout from 'layouts/Main';
 import SEO from 'components/SEO';
-import Hero from 'components/Sections/Hero';
 
+import Hero from 'components/Sections/Hero';
+import Annoucements from 'components/Sections/Annoucements';
 import EventSection from 'components/Sections/Events';
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Strona główna" />
 		<Hero />
+		<Annoucements />
 		<EventSection />
 	</Layout>
 );
