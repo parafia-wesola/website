08a70177f3cf2cd6ad9f9cdccc1649a3852c1e86
misioq23 <mi23chal@gmail.com>
2020-02-04 14:43:08 +0100

    add Modal component

---
+++ src/components/Modal/styles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	position: relative;
+	min-height: 100%;
+	padding: 4em 1.5rem;
+
+	${({ theme }) => theme.mq.tabletMid} {
+		padding: 6em 4rem;
+	}
+`;
+
+export const StyledTitle = styled.h2`
+	margin: 0 0 2em;
+`;
+
+export const Background = styled.div`
+	position: absolute !important;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	z-index: -1;
+
+	/* bright overlay */
+	::before {
+		position: absolute;
+		z-index: 1;
+		width: 100%;
+		height: 100%;
+		background: ${({ theme }) => theme.colors.bright};
+		opacity: 0.85;
+		content: '';
+	}
+`;
+
+export const Content = styled.div`
+	color: ${({ theme }) => theme.colors.dark};
+
+	p {
+		margin-bottom: 2em;
+		line-height: 1.5;
+	}
+`;
