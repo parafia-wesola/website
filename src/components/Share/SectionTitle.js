3356b6bbf5ae621965fdde59ecb8f9aa84755a37
misioq23 <mi23chal@gmail.com>
2020-02-05 21:31:28 +0100

    PAR-65 change SectionTitle font-size  & margin

--- src/components/Share/SectionTitle.js
+++ src/components/Share/SectionTitle.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const SectionTitle = styled.h2`
-	margin: 0 auto 1em;
+	margin: 0 auto 2em;
 	color:
 		${({ theme,
 		dark }) => dark ? theme.colors.dark : theme.colors.bright};
-	font-size: 2em;
+	font-size: 1.75em;
 	text-align: center;
 	text-transform: uppercase;
 	${({ hidden }) => hidden
