71b13652eb34175d464c90aeb15d639480c49f8f
misioq23 <mi23chal@gmail.com>
2020-02-09 15:24:30 +0100

    refactor global styles

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
+import './fonts.css';
+import './modal.css';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -17,39 +19,6 @@ const GlobalStyle = createGlobalStyle`
 	*::before,
 	*::after {
 		box-sizing: inherit;
-	}
-
-	.ReactModal__Html--open,
-	.ReactModal__Body--open {
-		overflow-y: hidden;
-	}
-
-	.ReactModal__Overlay {
-		position: fixed;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		z-index: 1000;
-		margin: 0 auto;
-		background-color: rgba(255, 255, 255, 0.75);
-	}
-
-	.ReactModal__Content {
-		position: absolute;
-		top: 50%;
-		right: 0;
-		left: 0;
-		max-width: 1360px;
-		height: 80vh;
-		margin: 0 auto;
-		overflow: auto;
-		background: #fff;
-		border: 4px solid #000;
-		border-radius: 4px;
-		outline: none;
-		transform: translateY(-50%);
-		-webkit-overflow-scrolling: touch;
 	}
 `;
 
