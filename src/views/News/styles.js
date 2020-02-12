5d60fdb7d86e253f7e2d32381fda1a9474f159a8
misioq23 <mi23chal@gmail.com>
2020-02-12 14:48:39 +0100

    refactor: move query for NewsFeed to News View

--- src/views/News/styles.js
+++ src/views/News/styles.js
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 
-export const NewsFeedWrapper = styled.div`
-	display: flex;
-`;
-
 export const StyledNewsFeed = styled.div`
-	flex: 1;
 	padding: 1em 0.75rem;
 `;
+
+export default StyledNewsFeed;
