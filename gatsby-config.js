38409c5c40ff7a3d9a83e68db794f2667db4287c
misioq23 <mi23chal@gmail.com>
2020-02-14 12:46:43 +0100

    add netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
 		'gatsby-plugin-styled-components',
 		'gatsby-transformer-json',
 		'gatsby-transformer-remark',
+		'gatsby-plugin-netlify-cms',
 		{
 			resolve: 'gatsby-plugin-modal-routing',
 			options: {
