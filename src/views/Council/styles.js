39a2ebe42e054d5673c343de2a0fd923a77b5d3a
misioq23 <mi23chal@gmail.com>
2020-02-22 15:49:11 +0100

    style: Council & Crew justify-content: evenly

--- src/views/Council/styles.js
+++ src/views/Council/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.ul`
-	justify-content: space-around;
+	justify-content: space-evenly;
 	margin: 3em auto 0;
 	padding: 0 1.5rem;
 `;
