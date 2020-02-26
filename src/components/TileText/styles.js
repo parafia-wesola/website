84fcf5946757337720d1b084ec8fd2ca9f1494db
misioq23 <mi23chal@gmail.com>
2020-02-26 10:55:36 +0100

    fix: TileText styles

--- src/components/TileText/styles.js
+++ src/components/TileText/styles.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	display: flex;
+	align-items: center;
 	justify-content: center;
-	width: 15em;
+	width: 18em;
+	height: 100%;
 	margin: 0;
 	color: #333;
 	line-height: 1em;
@@ -19,5 +21,7 @@ export const Wrapper = styled.div`
 `;
 
 export const Text = styled.p`
+	padding: 0 1.5rem;
 	font-weight: 600;
+	line-height: 1.5;
 `;
