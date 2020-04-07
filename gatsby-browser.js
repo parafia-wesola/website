1a97435b386c1e3f0ad31a9bf9f2ce8733545ac8
Michal Pawluczuk <mi23chal@gmail.com>
2020-04-07 09:45:40 +0200

    fix: scroll link with gatsby modal

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 /* eslint react/prop-types:0 */
+/* eslint consistent-return:0 */
 const React = require('react');
 const Layout = require('./src/layouts/Main/index').default;
 const { getOffset, smoothScroll } = require('./src/utils/index');
@@ -11,14 +12,16 @@ exports.wrapPageElement = ({ element }) => <Layout>{element}</Layout>;
 exports.shouldUpdateScroll = ({ routerProps: { location } }, opts = {}) => {
 	const { offsetElement = '#navbar', duration = 1000 } = opts;
 
-	const destinationElement = location.hash
-		? document.querySelector(location.hash)
-		: null;
-	if (!destinationElement) return true;
+	if (location.hash.includes('#')) {
+		const destinationElement = location.hash
+			? document.querySelector(location.hash)
+			: null;
+		if (!destinationElement) return true;
 
-	const destination = destinationElement.getBoundingClientRect().top;
-	const offset = getOffset(offsetElement);
+		const destination = destinationElement.getBoundingClientRect().top;
+		const offset = getOffset(offsetElement);
 
-	smoothScroll(destination, offset, duration);
-	return false;
+		smoothScroll(destination, offset, duration);
+		return false;
+	}
 };
