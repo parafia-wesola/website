5c2b59e5b2a4a4b63e426d7b98e6044a4d56cf63
Michal Pawluczuk <mi23chal@gmail.com>
2020-05-21 18:28:50 +0200

    fix: close button

--- src/components/Close/styles.js
+++ src/components/Close/styles.js
@@ -2,32 +2,38 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.button`
 	position: absolute;
-	top: 1em;
-	right: 1em;
+	top: 2em;
+	right: 2em;
 	z-index: 1001;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	width: 2em;
-	height: 2em;
+	width: 3em;
+	height: 3em;
 	padding: 0;
+	font-size: 11px;
 	background: transparent;
 	border: none;
 	cursor: pointer;
-	transition: transform 0.4s ease-in;
+	transition: transform 0.2s ease-out;
 
-	:active {
-		transform: rotate(360deg) scale(0);
+	:hover {
+		transform: scale(1.2);
 	}
 `;
 
 export const Cross = styled.span`
 	position: relative;
-	width: 2em;
+	width: 100%;
 	height: 0.4em;
 	background: ${({ theme }) => theme.colors.dark};
 	transform: rotate(135deg);
 	cursor: pointer;
+	transition: transform 0.2s ease-in;
+
+	:active {
+		transform: rotate(360deg) scale(0);
+	}
 
 	::before {
 		position: absolute;
