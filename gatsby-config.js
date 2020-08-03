4651ec56c2755d832ccfd6fb238a9f5049f9f443
Michal Pawluczuk <mi23chal@gmail.com>
2020-08-03 22:20:35 +0200

    fix: auto refresh

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,23 +79,6 @@ module.exports = {
 			},
 		},
 		{
-			resolve: 'gatsby-plugin-netlify',
-			options: {
-				headers: {
-					'/public/**/*.html': [
-						'cache-control: public',
-						'cache-control:  max-age=0',
-						'cache-control: must-revalidate',
-					],
-					'/public/page-data/*': [
-						'cache-control: public',
-						'cache-control:  max-age=0',
-						'cache-control: must-revalidate',
-					],
-				},
-			},
-		},
-		{
 			resolve: 'gatsby-plugin-manifest',
 			options: {
 				name:
