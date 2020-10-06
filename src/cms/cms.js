d3ebf293f53fbcb4e22457ea0c6e05c49f48f4df
Michal Pawluczuk <mi23chal@gmail.com>
2020-10-06 12:50:06 +0200

    feat: add annoucement preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,6 @@
 import CMS from 'netlify-cms-app';
 import ArticlePreview from './ArticlePreview';
+import AnnoucementPreview from './AnnoucementPreview';
 
 CMS.registerPreviewTemplate('articles', ArticlePreview);
+CMS.registerPreviewTemplate('annoucements', AnnoucementPreview);
