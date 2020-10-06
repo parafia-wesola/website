3f5d9bcac9c7a49b1d791da9871144270f51e095
Michal Pawluczuk <mi23chal@gmail.com>
2020-10-06 15:30:59 +0200

    fix: Close component rotate on click

--- src/components/Close/styles.js
+++ src/components/Close/styles.js
@@ -1,24 +1,28 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.button`
+	display: flex;
 	position: absolute;
+	z-index: 1001;
 	top: 2em;
 	right: 2em;
-	z-index: 1001;
-	display: flex;
 	align-items: center;
 	justify-content: center;
 	width: 3em;
 	height: 3em;
 	padding: 0;
-	font-size: 11px;
-	background: transparent;
-	border: none;
-	cursor: pointer;
 	transition: transform 0.2s ease-out;
+	border: none;
+	background: transparent;
+	font-size: 11px;
+	cursor: pointer;
 
 	:hover {
 		transform: scale(1.2);
+	}
+
+	&:active span {
+		transform: rotate(360deg) scale(0);
 	}
 `;
 
@@ -26,22 +30,18 @@ export const Cross = styled.span`
 	position: relative;
 	width: 100%;
 	height: 0.4em;
-	background: ${({ theme }) => theme.colors.dark};
 	transform: rotate(135deg);
-	cursor: pointer;
 	transition: transform 0.2s ease-in;
-
-	:active {
-		transform: rotate(360deg) scale(0);
-	}
+	background: ${({ theme }) => theme.colors.dark};
+	cursor: pointer;
 
 	::before {
-		position: absolute;
+		content: '';
 		display: block;
+		position: absolute;
 		width: 100%;
 		height: 0.4em;
-		background: inherit;
 		transform: rotate(90deg);
-		content: '';
+		background: inherit;
 	}
 `;
