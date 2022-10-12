d50092e827869504c9f0b95c798c2cb4b059fb3c
parafia-wesola <61565317+parafia-wesola@users.noreply.github.com>
2022-10-13 00:02:43 +0200

    Add fixWYSIWYG to cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,9 @@
 import CMS from 'netlify-cms-app';
 import ArticlePreview from './ArticlePreview';
 import AnnoucementPreview from './AnnoucementPreview';
+import { fixWYSIWYG } from './fixwysiwyg';
+
+fixWYSIWYG();
 
 CMS.registerPreviewTemplate('articles', ArticlePreview);
 CMS.registerPreviewTemplate('annoucements', AnnoucementPreview);
